5ec44e06438fe5d88d8d6853da31c49d9796a88a
SOURABH SINGH <94341076+rajpootsourabh@users.noreply.github.com>
2024-03-09 18:49:23 +0530

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,38 +4,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class CalculatorTest {
-    
+    Calculator calculator = new Calculator();
     @Test
     public void testAdd() {
-        Calculator calculator = new Calculator();
+        
         assertEquals(5, calculator.add(2, 3));
         assertEquals(-1, calculator.add(-5, 4));
     }
     
     @Test
     public void testSubtract() {
-        Calculator calculator = new Calculator();
         assertEquals(1, calculator.subtract(4, 3));
         assertEquals(-6, calculator.subtract(-2, 4));
     }
     
     @Test
     public void testMultiply() {
-        Calculator calculator = new Calculator();
         assertEquals(6, calculator.multiply(2, 3));
         assertEquals(-8, calculator.multiply(-2, 4));
     }
     
     @Test
     public void testDivide() {
-        Calculator calculator = new Calculator();
         assertEquals(3, calculator.divide(9, 3));
         assertEquals(-2, calculator.divide(-10, 5));
     }
     
     @Test
     public void testDivideByZero() {
-        Calculator calculator = new Calculator();
         assertThrows(IllegalArgumentException.class, () -> calculator.divide(5, 0));
     }
 }
